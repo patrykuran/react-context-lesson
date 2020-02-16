146941e3264372eabb75bb39209d86c7be904c25
patrykuran <trykpa@gmail.com>
2020-02-16 17:01:57 +0100

    use context in cart

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import React, {useContext} from 'react';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
-import {
-  selectCartItems,
-  selectCartTotal
-} from '../../redux/cart/cart.selectors';
+import {selectCartTotal} from '../../redux/cart/cart.selectors';
+
+import {CartContext} from "../../providers/cart/cart.provider";
 
 import './checkout.styles.scss';
 
-const CheckoutPage = ({ cartItems, total }) => (
-  <div className='checkout-page'>
+const CheckoutPage = ({total}) => {
+  const {cartItems} = useContext(CartContext);
+
+  return (<div className='checkout-page'>
     <div className='checkout-header'>
       <div className='header-block'>
         <span>Product</span>
@@ -32,20 +33,19 @@ const CheckoutPage = ({ cartItems, total }) => (
       </div>
     </div>
     {cartItems.map(cartItem => (
-      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+        <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
     ))}
     <div className='total'>TOTAL: ${total}</div>
     <div className='test-warning'>
       *Please use the following test credit card for payments*
-      <br />
+      <br/>
       4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
     </div>
-    <StripeCheckoutButton price={total} />
-  </div>
-);
+    <StripeCheckoutButton price={total}/>
+  </div>)
+};
 
 const mapStateToProps = createStructuredSelector({
-  cartItems: selectCartItems,
   total: selectCartTotal
 });
 
